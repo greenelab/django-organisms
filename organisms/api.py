5d5558f0cef21573017c776e47e7b0d271ee72dc
rzelayafavila <Rene.Zelaya.F@gmail.com>
2016-06-21 16:36:21 -0400

    Moving OrganismResource class code outside of the try...except statements, and only leaving the django-tastypie imports inside those statements.

--- organisms/api.py
+++ organisms/api.py
@@ -7,15 +7,15 @@ logger.addHandler(logging.NullHandler())
 
 try:
     from tastypie.resources import ModelResource, ALL
-
-    class OrganismResource(ModelResource):
-
-        class Meta:
-            queryset = Organism.objects.all()
-            filtering = {'scientific_name': ALL, 'slug': ALL,
-                         'taxonomy_id': ALL}
-            allowed_methods = ['get']
-            detail_uri_name = 'slug'
-
 except ImportError:
     logger.info('Not using django-tastypie in organisms/api.py file')
+
+
+class OrganismResource(ModelResource):
+
+    class Meta:
+        queryset = Organism.objects.all()
+        filtering = {'scientific_name': ALL, 'slug': ALL,
+                     'taxonomy_id': ALL}
+        allowed_methods = ['get']
+        detail_uri_name = 'slug'
